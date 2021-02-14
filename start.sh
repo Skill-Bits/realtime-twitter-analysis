ebf789562dc81ee7f41e7ac449a50ccb0da50836
Tomasz Dłuski <mr.toumash@gmail.com>
2021-02-14 11:28:17 +0100

    better start.sh script

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-python ./1.ingest/twitter_ingest.py &
-python ./2.sentiment/sentiment.py &
-python ./3.persistence/persistence.py  &
+echo "[x] Rabbit http://localhost:8080"
+echo "[x] InfluxDB http://localhost:8888"
+echo "[x] Grafana http://localhost:5000"
+./wait-for-it.sh localhost:5672 # rabbitmq
+./wait-for-it.sh localhost:8086 # influxdn
+./wait-for-it.sh localhost:5000 # grafana
+
+python ./1.ingest/twitter_ingest.py & python ./2.sentiment/sentiment.py & python ./3.persistence/persistence.py
